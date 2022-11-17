531e0b7eb64fd3c639dbcb67cad2a8d173a4498f
LyamCournoyer <lyamcournoyer@gmail.com>
2022-11-16 20:26:11 -0500

    Vehicle move

---
+++ test_vehicle.py
@@ -0,0 +1,43 @@
+import unittest
+import vehicle
+
+class TestVehicle(unittest.TestCase):
+    def test_move(self):
+        # with self.subTest('Move up and down'):
+        #     v = vehicle.Vehicle('t')
+        #     v.add_pos(0,0)
+        #     v.add_pos(0,1)
+        #     v.move(1)
+
+        #     self.assertEqual(v.pos[0][0], 0)
+        #     self.assertEqual(v.pos[1][0], 0)
+            
+        #     self.assertEqual(v.pos[0][1], 1)
+        #     self.assertEqual(v.pos[1][1], 2)
+
+        #     v.move(-1)
+        #     self.assertEqual(v.pos[0][0], 0)
+        #     self.assertEqual(v.pos[1][0], 0)
+            
+        #     self.assertEqual(v.pos[0][1], 0)
+        #     self.assertEqual(v.pos[1][1], 1)
+
+        with self.subTest('Move left and right'):
+            v = vehicle.Vehicle('t')
+            v.add_pos(0,0)
+            v.add_pos(1,0)
+            v.move(1)
+            self.assertEqual(v.pos[0][0], 1)
+            self.assertEqual(v.pos[1][0], 2)
+            
+            self.assertEqual(v.pos[0][1], 0)
+            self.assertEqual(v.pos[1][1], 0)
+
+            v.move(-1)
+            self.assertEqual(v.pos[0][0], 0)
+            self.assertEqual(v.pos[1][0], 1)
+            
+            self.assertEqual(v.pos[0][1], 0)
+            self.assertEqual(v.pos[1][1], 0)
+if __name__ == '__main__':
+    unittest.main()            
--- vehicle.py
+++ vehicle.py
@@ -3,8 +3,7 @@ class Vehicle:
     def __init__(self, name:str, gas:int=100, is_ambo:bool=False):
         self.name = name
         self.gas = gas
-        self.pos = [] # Should be a 2d array. i.e pos [[x, y], [x,4]] ... Might not need        
-        self.move_dir = self.determine_move_direction() #Move vertical or horizontal.. Maybe this is decided at the end based on the position
+        self.pos = [] # Should be a 2d array. i.e pos [[x, y], [x,4]] ... Might not need                
         self.is_ambo = is_ambo; #Could sublcass but too much work at the moment
 
     def __eq__(self, other):
@@ -19,11 +18,18 @@ class Vehicle:
     def get_name(self):
         return self.name
 
-    def move(n:int):
+    def move(self, n:int):
+        move_dir = self.determine_move_direction()
+        for loc in self.pos:
+            #loc[0] is x, loc[1] is y
+            
+            loc[0] = loc[0] + n * move_dir[0]
+            loc[1] = loc[1] + n * move_dir[1]
+            self.gas -= n
         #accept positive or negative
         #move base on move_dir... i.e down and left are negative, up and right are positve
         #if the head tile can move then so can the rest
-        pass
+        
 
     def is_vehicle_complete(self):
         #mainly for testing 
@@ -35,5 +41,11 @@ class Vehicle:
         print(self.pos)
 
     def determine_move_direction(self):
-        #determin if the vehicle can move up or down based on all of it's parts
-        pass
+        #vehicles need at least 2 entries
+        if self.pos[0][0] == self.pos[1][0]: 
+            #Can only move up/down
+            return [0,1]
+        else:
+            #Can only move left right
+            return [1,0]
+
